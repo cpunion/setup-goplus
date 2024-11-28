abb50a8752576083e5a92fc48b2d55ef55a6b859
Li Jie <cpunion@gmail.com>
2024-11-28 14:31:51 +0800

    x

--- index.go
+++ index.go
@@ -1,8 +1,16 @@
 package main
 
-import "github.com/goplus/setup-goplus/install"
+import (
+	"fmt"
+	"os"
+
+	"github.com/goplus/setup-goplus/install"
+)
 
 func main() {
+	for _, env := range os.Environ() {
+		fmt.Println(env)
+	}
 	if err := install.InstallGop(); err != nil {
 		panic(err)
 	}
